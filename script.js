5c242e8abbc33a96914f7c44a97a9440c3bab7e5
creyes25 <carolina.reyes2022@gmail.com>
2023-10-18 23:38:22 -0500

    add functionality and style so when hovering text it gives a different look

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const circle = document.querySelector('.circle')
 const circleStyle = circle.style
+const textHover = document.querySelector('h2')
+let mouseMoving = false
 
+setInterval(() => {
+  if (mouseMoving === true) {
+    mouseMoving = false
+    circleStyle.transform = `scale(1)`
+  }
+}, 1000)
+
+
+// when mouse moves, the circle will move
 document.addEventListener('mousemove', (evt) => {
-  circleStyle.top = `${evt.clientY}px`
-  circleStyle.left = `${evt.clientX}px`
-
+  circleStyle.top = `calc(${evt.clientY}px - 52vh)`
+  circleStyle.left = `calc(${evt.clientX}px )`
+  circleStyle.transform = `scale(1.6)`
+  mouseMoving = true
 })
+
